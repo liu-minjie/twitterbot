02d866286615871e4369d9527cfa68106ae77ec5
liu-minjie <minjieliu@163.com>
2018-02-16 14:21:39 +0800

    up

--- reply.js
+++ reply.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const request = require('request');
 
 const screen_name = 'sarah_edo';
@@ -5,18 +6,47 @@ const id_str = '963462632693903360';
 
 let save = [];
 
+
+function handleData( ){
+	data = data.split('tweet js-stream-tweet');
+	data.forEach((item) => {
+		const reply = {};
+		const conent = data.match(/<p class=\"TweetTextSize  js-tweet-text tweet-text\".+?(?=<\/p>)/g) || [];
+		const names = data.match(/<span class=\"username u-dir u-textTruncate\".+?<b>(.+?)<\/b>/);
+
+		reply.content = content.match(/<p class=\"TweetTextSize  js-tweet-text tweet-text\".+?>(.+)/)[1];
+
+		if (names.length) {
+			reply.screen_name = names[0].match(/<b>(.+?)<\/b>/)[1];
+			if (names.length > 1) {
+				reply.to = [];
+				for (let i = 1; i < names.length; i++) {
+					reply.to.push(names[0].match(/<b>(.+?)<\/b>/)[1])
+				}
+			}
+		}
+
+		save.push(reply);
+	});
+}
+
 request({
 	url: `https://twitter.com/${screen_name}/status/${id_str}`
 }, (err, res, data) => {
 
+	data = data.split('id="descendants"')[1];
+	data = data.split('hidden-replies-container')[0];
+
 	data = data.replace(/\n/g, '');
-	const reply = data.match(/<p class=\"TweetTextSize  js-tweet-text tweet-text\".+?(?=<\/p>)/g) || [];
+	handleData(data);
 
 	const match = data.match(/data-min-position=\"([^\"]+)/);
 
 	if (!match) {
 		save = save.concat(reply);
-		fs.writeFileSync(`./reply/${screen_name}.json`, JSON.stringify(save, null, 1), 'utf8');
+		fs.writeFileSync(`./reply/${screen_name}.json`, JSON.stringify({
+			[id_str]: save
+		}, null, 1), 'utf8');
 		return;
 	}
 	next(match[1]);
@@ -27,11 +57,13 @@ function next (max_position) {
 	request({
 		url: `https://twitter.com/i/${screen_name}/conversation/${id_str}?max_position=${max_position}&include_available_features=1&include_entities=1&reset_error_state=false`
 	}, (err, res, data) => {
-		data = data.descendants;
+		data = JSON.parse(data);
 
-		const reply = data.items_html.match(/<p class=\"TweetTextSize  js-tweet-text tweet-text\".+?(?=<\/p>)/g) || [];
-		save = save.concat(reply);
-		fs.writeFileSync(`./reply/${screen_name}.json`, JSON.stringify(save, null, 1), 'utf8');
+		handleData(data.items_html);
+		fs.writeFileSync(`./reply/${screen_name}.json`, JSON.stringify({
+			[id_str]: save
+		}, null, 1), 'utf8');
+		
 		if (data.has_more_items) {
 			setTimeout(() => {
 				//next(data.min_position)
