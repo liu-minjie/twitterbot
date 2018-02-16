2b853a2a6b372f630e2d3011eb0ba78278d96ab0
liu-minjie <minjieliu@163.com>
2018-02-16 12:14:15 +0800

    up

--- img.js
+++ img.js
@@ -32,3 +32,7 @@ async.eachSeries(files, (filename, next) => {
 	fs.writeFileSync('./stat/cache_avatar.json', JSON.stringify(save, null, 1), 'utf8');
 	console.log(err, 'done');
 });
+
+const  download = function(uri, filename, callback){
+  request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+};
