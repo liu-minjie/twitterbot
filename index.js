e20d223716de3226c7e702374764cb0051d80c37
liu-minjie <minjieliu@163.com>
2018-04-01 11:07:23 -0400

    up

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ function getStatusesUserTimeline(count, ret, username, max_id, since_id, next) {
 
 	  if (error) {
 	  	if (error.message.indexOf('401 Auth') != -1) {
+console.log(username, 401)
 	  		return  saveTimeline(username, ret, count, next);
 	  	}
 	  	if (count > 14) {
